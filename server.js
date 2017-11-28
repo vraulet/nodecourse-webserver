08b1b00081b99c296a3143ad0d25238547bb15a4
Valery Raulet <vraulet@users.noreply.github.com>
2017-11-28 09:36:30 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs')
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'www')));
+app.use((request, response, next) => {
+    var now = new Date().toString();
+    var log = `Page requested ${request.method} - ${request.url} at ${now}`;
+    console.log(log);
+    fs.appendFile('server.log', log +'\n', (err) => {
+        if (err) {
+            console.log('error');
+        }
+    });
+    next();
+});
+
+app.use((request, response, next) => {
+    response.render('maintenance.hbs');
+    
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+// app.get('/', (request, response) => {
+//     // response.send('<h1>Hello <b>Express</b></h1>');
+//     response.send({name: 'Valery', age: 25});
+// });
+
+app.get('/', (request, response) => {
+    response.render('home.hbs', {
+        pageTitle: 'Homepage HBS',
+        welcomeMessage: 'Welcome to our super duper page'
+    });
+});
+
+app.get('/about', (request, response) => {
+    response.render('about.hbs', {
+        pageTitle: 'About page HBS'
+    });
+});
+
+app.get('/bad', (request, response) => {
+    response.send({errorMessage: 'Unable to handle request'});
+});
+app.listen(3000, () => {
+    console.log('server is up, port 3000.');
+});
